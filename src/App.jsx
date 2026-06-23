c5485a1f2be08dd1c6f7cfd1c0c7b0d273a871f0
pavithra <pavithraramar537@gmail.com>
2026-06-23 20:12:09 +0530

    remove contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
-import Contact from './pages/Contact'
+import Contact from './pages/Contact';
 
 function App() {
   return (
